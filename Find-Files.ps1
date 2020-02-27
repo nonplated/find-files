b3dddfbffe25d76738d409af37d21588df5aa528
Mac <40839417+nonplated@users.noreply.github.com>
2020-02-27 18:28:53 +0100

    changed help description

--- Find-Files.ps1
+++ Find-Files.ps1
@@ -1,34 +1,94 @@
-﻿<#
-    (Powershell)
-
-    Function to find some files (pattern like *.*) in specified directories (with all subdirectories)
-
-    See at bottom for examples
-
-    Tested on Windows 10 
-    
-    Winter, 2020 (Not So Cold)
-#>
+﻿
 
 function Find-Files {
-    
+
+<#
+.SYNOPSIS
+Function to find some files (pattern like *.*) in specified directories (with all subdirectories)
+
+.DESCRIPTION
+Tested on Windows 10
+Winter, 2020 (Not So Cold)
+
+.PARAMETER SearchPattern
+Pattern of files to find: *.*, *.bmp
+
+.PARAMETER Dirs
+The directories where look.
+
+.PARAMETER PrintFileNames
+Set if filenames should be outputed.
+
+.PARAMETER RemovableDrives
+Set to search files on USB,CD,DVD drives too.
+
+.PARAMETER NoSummary
+Dont show calculating about found files.
+
+.EXAMPLE
+PS > Find-Files -PrintFileNames *.mp4
+
+.EXAMPLE
+PS > Find-Files -?
+
+.EXAMPLE
+PS > Find-Files *.pdf
+
+Search all fixed drives (without removable drives)
+
+.EXAMPLE
+PS > Find-Files @("*.txt","*.log")
+
+Search txt and log files
+
+.EXAMPLE
+PS > Find-Files *.pdf -PrintFileNames
+
+Search all fixed drives and plot file names (full path)
+
+.EXAMPLE
+PS > Find-Files *.pdf -Dirs @("C:\Windows","C:\Users")
+
+Search only selected directories (with all subdirs)
+
+.EXAMPLE
+PS > Find-Files *.pdf -RemovableDrives
+
+Search all drives included removable (like CD-ROM, USB-Flash)
+
+.EXAMPLE
+PS > Find-Files *.pdf -PrintFileNames | Out-File -FilePath "C:\output.txt"
+
+if you want to save filenames output to file
+
+.EXAMPLE
+PS > Find-Files *.pdf -PrintFileNames -NoSummary | Out-File -FilePath "C:\output_no_summary.txt"
+
+Save filenames, but with no summary
+
+.LINK
+https://github.com/nonplated/powershell
+#>
+
     param (
-        $SearchPattern,
-        $Dirs="",
+        [Parameter(Mandatory=$true)][string]$SearchPattern,
+        [string[]]$Dirs="",
         [switch]$PrintFileNames=$false,
         [switch]$RemovableDrives=$false,
-        [switch]$Summary=$true
+        [switch]$NoSummary=$false
     )
 
     if (!$Dirs) {
         # get all volumes letters (only volumes with letter and not removable)
-        $Dirs = Get-Volume | 
-            Where-Object { 
-                $_.DriveLetter -and ($RemovableDrives -or ($_.DriveType -eq "fixed")) 
-            } | 
-            ForEach-Object { 
-                $_.DriveLetter+":\" 
+        $Dirs = Get-Volume `
+            | Where-Object {
+                $_.DriveLetter -and ($RemovableDrives -or ($_.DriveType -eq "fixed"))
+            } `
+            | Sort-Object -unique DriveLetter `
+            | ForEach-Object {
+                $_.DriveLetter+":\"
             }
+        Write-Host "Found drives: $Dirs"
     }
 
     Write-Host "Looking for files ($SearchPattern) in directories ($Dirs)..."
@@ -37,13 +97,13 @@ function Find-Files {
     ForEach ($Dir in $Dirs) {
         $TotalLength = 0 #file size [B]
         $TotalFiles = 0
-        Get-Childitem $Dir -Include $SearchPattern -Recurse -ErrorAction SilentlyContinue | 
-            ForEach-Object { 
-                $TotalLength += $_.Length; 
-                $TotalFiles += 1; 
-                if ($PrintFileNames) { "$_" }; 
+        Get-Childitem $Dir -Include $SearchPattern -Recurse -ErrorAction SilentlyContinue `
+            | ForEach-Object {
+                $TotalLength += $_.Length;
+                $TotalFiles += 1;
+                if ($PrintFileNames) { "$_" };
             }
-        if ($Summary) {
+        if (!$NoSummary) {
             $TotalLength = ($TotalLength/(1024*1024)).ToString('0') #convert B to MB
             Write-Host ""
             Write-Host " # In directory: $Dir"
@@ -52,26 +112,3 @@ function Find-Files {
         }
     }
 }
-
-
-########################
-####    Examples    ####
-########################
-
-Find-Files *.pdf
-#search all fixed drives (without removable drives)
-
-Find-Files *.pdf -PrintFileNames
-#search all fixed drives and plot file names (full path)
-
-Find-Files *.pdf -Dirs @("C:\Windows","C:\Users") 
-#search only selected directories (with all subdirs)
-                                              
-Find-Files *.pdf -RemovableDrives           
-#search all drives included removable (like CD-ROM, USB-Flash)
-
-Find-Files *.pdf -PrintFileNames | Out-File -FilePath "C:\output.txt" 
-#if you want to save filenames output to file
-
-Find-Files *.pdf -PrintFileNames -Summary:$false | Out-File -FilePath "C:\output_no_summary.txt"
-#save filenames, but with no summary
